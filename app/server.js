1a7d7c0c3c8c9ead3c853abd5fe2fc97e9106546
Rana Salem <ranatasalem@gmail.com>
2024-08-15 00:10:39 +0100

    update server

--- app/server.js
+++ app/server.js
@@ -1,18 +1,3 @@
-// const Hapi = require('@hapi/hapi')
-
-// const server = Hapi.server({
-//   port: '3001'
-// })
-
-// const routes = [].concat(
-//   require('./routes/healthy'),
-//   require('./routes/healthz')
-// )
-
-// server.route(routes)
-
-// module.exports = { server }
-
 const express = require('express')
 const healthyRouter = require('./routes/healthy')
 const healthzRouter = require('./routes/healthz')
